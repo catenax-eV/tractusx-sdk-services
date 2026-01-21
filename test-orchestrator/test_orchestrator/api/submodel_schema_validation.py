fdc1807e10f49efd3488e6218d413e92ab166746
asmierzchalski <adam.smierzchalski@doubleslash.de>
2026-01-21 15:53:11 +0100

    Remove redundant utility files `base_utils.py` and `utils.py` in test orchestrator module

--- test-orchestrator/test_orchestrator/api/submodel_schema_validation.py
+++ test-orchestrator/test_orchestrator/api/submodel_schema_validation.py
@@ -44,7 +44,7 @@ from typing import Dict
 from fastapi import APIRouter, Depends
 
 from test_orchestrator.auth import verify_auth
-from test_orchestrator.base_utils import submodel_validation
+from test_orchestrator.baseutils import submodel_validation
 from test_orchestrator.utils.submodel_schema_validation import (
     process_and_retrieve_dtr,
 )
