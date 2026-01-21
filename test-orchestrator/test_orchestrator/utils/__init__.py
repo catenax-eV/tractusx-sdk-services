20599411f4160ba320887c07cffdfebd44f0d843
asmierzchalski <adam.smierzchalski@doubleslash.de>
2026-01-21 16:08:08 +0100

    Remove redundant utility files `base_utils.py` and `utils.py` in test orchestrator module

--- test-orchestrator/test_orchestrator/utils/__init__.py
+++ test-orchestrator/test_orchestrator/utils/__init__.py
@@ -19,16 +19,3 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # *************************************************************
-
-# Load utils.py directly (folder takes precedence over file, so we use importlib)
-import importlib.util
-import os
-
-_utils_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'baseutils.py')
-_spec = importlib.util.spec_from_file_location("_baseutils_module", _utils_path)
-_utils = importlib.util.module_from_spec(_spec)
-_spec.loader.exec_module(_utils)
-
-init_negotiation = _utils.init_negotiation
-obtain_negotiation_state = _utils.obtain_negotiation_state
-get_data_address = _utils.get_data_address
